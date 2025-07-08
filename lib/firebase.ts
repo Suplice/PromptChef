aa5dad3c36d8da3b92fe028600a8c7c8a67d74e0
Mateusz Duma <mateuszsuplice@gmail.com>
2025-07-08 14:33:52 +0200

    feat: add measurementId to Firebase configuration for enhanced analytics

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
